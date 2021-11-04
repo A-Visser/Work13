00c8391250f854179311aca3d9017bbdbb63fcb3
A-Visser <91136835+A-Visser@users.noreply.github.com>
2021-11-04 10:55:48 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,7 @@
+/* Use sscanf to read in the data
+create struct and array of structs 
+use this to recreate csv file in other file
+read from data file back into structs
+use fgets sscanf and printf to get data from the user, put it into struct, add to data file
 
+*/
